a905fbb340984d30699ea22a45f79e5626b2f549
michal-wojcik <6065832+michal-wojcik@users.noreply.github.com>
2023-05-02 21:50:13 +1000

    Add maven structure

---
+++ src/test/java/au/michalwojcik/messaging/MessageAnnotatorApplicationTests.java
@@ -0,0 +1,13 @@
+package au.michalwojcik.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageAnnotatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
